dc51f7621ac81162242431d18f529fcf4e1a5208
Avdhoot <handeavdhoot03@gmail.com>
2026-01-27 09:11:14 +0530

    updated the backend url

--- FRONT/my-app/app/api/predict/route.ts
+++ FRONT/my-app/app/api/predict/route.ts
@@ -4,7 +4,7 @@ export async function POST(req: Request) {
   try {
     const body = await req.json();
 
-    const backendRes = await fetch("http://localhost:5000/predict", {
+    const backendRes = await fetch(process.env.BACKEND!, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(body),
